da2c383711b68e4cfd98e31bb00b90ecb4f6d61d
Loquitos13 <dbp13@ua.pt>
2025-04-30 20:12:30 +0100

    logica para opcao marcada do menu

---
+++ app/src/main/java/com/example/shoppingwise2/ProfileActivity.java
@@ -0,0 +1,42 @@
+package com.example.shoppingwise2;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setItemIconTintList(null);
+        bottomNavigationView.setItemTextColor(getResources().getColorStateList(R.color.nav_icon_color));
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.nav_history) {
+                    return true;
+                } else if (itemId == R.id.nav_profile) {
+                    return true;
+                } else if (itemId == R.id.nav_home) {
+                    return true;
+                } else if (itemId == R.id.nav_search) {
+                    return true;
+                } else if (itemId == R.id.nav_scan) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+
+        bottomNavigationView.setSelectedItemId(R.id.nav_profile);
+    }
+}
