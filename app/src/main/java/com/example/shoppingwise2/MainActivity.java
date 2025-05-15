664137aff96a3082337a4a16e55de22a91904964
unknown <brunolopesjardim@ua.pt>
2025-05-15 09:59:18 +0100

    Autenticação 15/05 10:00

--- app/src/main/java/com/example/shoppingwise2/MainActivity.java
+++ app/src/main/java/com/example/shoppingwise2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.shoppingwise2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -10,20 +11,34 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int CAMERA_REQUEST_CODE = 100;
     private Button scanButton;
+    boolean isAuthenticated;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences prefs = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        isAuthenticated = prefs.getBoolean("isLoggedIn", false);
+        scanButton = findViewById(R.id.btn_comecar);
 
-      scanButton = findViewById(R.id.btn_comecar);
+        if (!isAuthenticated) {
+            if (userJaRegistrado()) {
+                startActivity(new Intent(this, LoginActivity.class));
+            } else {
+                startActivity(new Intent(this, SignInActivity.class));
+            }
+        }
 
-        // Abre diretamente a ScannerActivity ao clicar no botão
         scanButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, ScannerActivity.class);
             startActivity(intent);
         });
 
+    }
+
+    public boolean userJaRegistrado() {
+        SharedPreferences prefs = getSharedPreferences("user_prefs", MODE_PRIVATE);
+        return prefs.getBoolean("ja_registrado", false);
     }
 }
